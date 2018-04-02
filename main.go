070add87d87fed008ab7db274e37a2a1691e6030
Christopher Loessl <cloessl+github@gmail.com>
2018-04-02 21:15:23 +0200

    Update: Sample code from doc
    instead of a profiling page on the internet.

--- main.go
+++ main.go
@@ -157,10 +157,13 @@ func memProfile() {
 	fmt.Println("Writing memory profile to", config.memProfileFile)
 	f, err := os.Create(config.memProfileFile)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("could not create memory profile: ", err)
 	}
-	defer f.Close()
-	pprof.WriteHeapProfile(f)
+	runtime.GC() // get up-to-date statistics
+	if err := pprof.WriteHeapProfile(f); err != nil {
+		log.Fatal("could not write memory profile: ", err)
+	}
+	f.Close()
 }
 
 func exampleColorImages() {
@@ -204,10 +207,11 @@ func main() {
 		fmt.Println("Starting CPU profiling to file", config.cpuProfileFile)
 		f, err := os.Create(config.cpuProfileFile)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("could not create CPU profile: ", err)
 		}
-		defer f.Close()
-		pprof.StartCPUProfile(f)
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
 		defer pprof.StopCPUProfile()
 	}
 
